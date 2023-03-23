3b80436ad215577a27b988d2f4e1369a70d23b99
lroncevic <roncevicluka@outlook.com>
2023-03-23 14:34:22 +0100

    Added http post for Customer

--- src/main/java/com/lukaroncevic/spring6restmvc/controller/CustomerController.java
+++ src/main/java/com/lukaroncevic/spring6restmvc/controller/CustomerController.java
@@ -3,10 +3,10 @@ package com.lukaroncevic.spring6restmvc.controller;
 import com.lukaroncevic.spring6restmvc.model.Customer;
 import com.lukaroncevic.spring6restmvc.services.CustomerService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +17,17 @@ import java.util.UUID;
 public class CustomerController {
 
     private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Customer customer){
+
+        Customer savedCustomer = customerService.saveNewCustomer(customer);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "/api/v1/customer/" + savedCustomer.getId().toString());
+
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Customer> listCustomers(){
--- src/main/java/com/lukaroncevic/spring6restmvc/services/CustomerService.java
+++ src/main/java/com/lukaroncevic/spring6restmvc/services/CustomerService.java
@@ -9,4 +9,5 @@ public interface CustomerService {
 
     List<Customer> listCustomers();
     Customer getCustomerById(UUID id);
+    Customer saveNewCustomer(Customer customer);
 }
