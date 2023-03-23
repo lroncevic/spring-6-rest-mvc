959c2bb4681fb8c4d9cf3ee70eb5717cab019fee
lroncevic <roncevicluka@outlook.com>
2023-03-23 09:33:05 +0100

    Added constructor

---
+++ src/main/java/com/lukaroncevic/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,12 @@
+package com.lukaroncevic.spring6restmvc.controller;
+
+import com.lukaroncevic.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+
+    private final BeerService beerService;
+}
