d49fcb723801f94c09e44426cb033fe76fca4ce6
lroncevic <roncevicluka@outlook.com>
2023-03-25 21:20:26 +0100

    Added hibernate UUID generation

--- src/main/java/com/lukaroncevic/spring6restmvc/entities/Beer.java
+++ src/main/java/com/lukaroncevic/spring6restmvc/entities/Beer.java
@@ -1,2 +1,34 @@
-package com.lukaroncevic.spring6restmvc.entities;public class Beer {
+package com.lukaroncevic.spring6restmvc.entities;
+
+import com.lukaroncevic.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator ")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
 }
--- src/main/java/com/lukaroncevic/spring6restmvc/entities/Customer.java
+++ src/main/java/com/lukaroncevic/spring6restmvc/entities/Customer.java
@@ -1,2 +1,28 @@
-package com.lukaroncevic.spring6restmvc.entities;public class Customer {
+package com.lukaroncevic.spring6restmvc.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator ")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    private String name;
+    @Version
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 }
