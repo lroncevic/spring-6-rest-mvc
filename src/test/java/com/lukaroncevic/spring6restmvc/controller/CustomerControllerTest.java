6a29ec1df9740b62d628553eff1694b20baa0dee
lroncevic <roncevicluka@outlook.com>
2023-03-25 09:45:45 +0100

    Added delete Customer test

--- src/test/java/com/lukaroncevic/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/com/lukaroncevic/spring6restmvc/controller/CustomerControllerTest.java
@@ -7,6 +7,8 @@ import com.lukaroncevic.spring6restmvc.services.CustomerService;
 import com.lukaroncevic.spring6restmvc.services.CustomerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -36,24 +39,41 @@ class CustomerControllerTest {
 
     CustomerServiceImpl customerServiceImpl;
 
+    @BeforeEach
+    void setUp(){
+
+        customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+    @Test
+    void deleteCustomer() throws Exception {
+
+        Customer customer = customerServiceImpl.listCustomers().get(0);
+
+        mockMvc.perform(delete("/api/v1/customer/" + customer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(customerService).deleteById(uuidArgumentCaptor.capture());
+
+        assertThat(customer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+    }
+
     @Test
     void updateCustomer() throws Exception {
 
         Customer customer = customerServiceImpl.listCustomers().get(0);
 
         mockMvc.perform(put("/api/v1/customer/" + customer.getId())
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(customer)))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isNoContent());
 
         verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
-    }
-
-    @BeforeEach
-    void setUp(){
-
-        customerServiceImpl = new CustomerServiceImpl();
     }
 
     @Test
