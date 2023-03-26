ccaa83ad0992d568ae8213299ae5cd1790160b5e
lroncevic <roncevicluka@outlook.com>
2023-03-26 12:59:54 +0200

    JPA Get Operations

--- src/main/java/com/lukaroncevic/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/lukaroncevic/spring6restmvc/services/BeerServiceJPA.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -20,16 +21,20 @@ public class BeerServiceJPA implements BeerService {
     private final BeerMapper beerMapper;
     @Override
     public List<BeerDTO> listBeers() {
-        return null;
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<BeerDTO> getBeerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(beerMapper.beerToBeerDTO(beerRepository.findById(id)
+                .orElse(null)));
     }
 
     @Override
-    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+    public BeerDTO saveNewBeer(BeerDTO beerDTO)  {
         return null;
     }
 
