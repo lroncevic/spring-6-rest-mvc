935e5b5269a4ec0be4956da16cd4d54da0980539
lroncevic <roncevicluka@outlook.com>
2023-03-27 18:42:19 +0200

    Added controller binding validation

--- src/test/java/com/lukaroncevic/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/lukaroncevic/spring6restmvc/controller/BeerControllerTest.java
@@ -109,6 +109,20 @@ class BeerControllerTest {
     }
 
     @Test
+    void testCreateBeerNullBeerName() throws Exception {
+
+        BeerDTO beerDTO = BeerDTO.builder().build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(post(BeerController.BEER_PATH)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
     void createNewBeer() throws Exception {
 
        BeerDTO beerDTO = beerServiceImpl.listBeers().get(0);
