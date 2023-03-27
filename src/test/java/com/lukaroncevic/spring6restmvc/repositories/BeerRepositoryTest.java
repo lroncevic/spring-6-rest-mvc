7721cc2a4c6b696e9e2510d05c3598b6f0703f9b
lroncevic <roncevicluka@outlook.com>
2023-03-27 20:06:00 +0200

    Added JPA validation

--- src/test/java/com/lukaroncevic/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/lukaroncevic/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.lukaroncevic.spring6restmvc.repositories;
 
 import com.lukaroncevic.spring6restmvc.entities.Beer;
+import com.lukaroncevic.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +22,12 @@ class BeerRepositoryTest {
 
         Beer savedBeer = beerRepository.save(Beer.builder()
                 .beerName("My Beer")
+                        .beerStyle(BeerStyle.PALE_ALE)
+                        .upc("1245124")
+                        .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
