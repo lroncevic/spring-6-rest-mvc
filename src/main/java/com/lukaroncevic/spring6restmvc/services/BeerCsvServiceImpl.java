e2ca09bdf21b2d5a920064b7ed9c776c502af295
lroncevic <roncevicluka@outlook.com>
2023-03-28 20:06:13 +0200

    Added csv load to bootstrap

--- src/main/java/com/lukaroncevic/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/com/lukaroncevic/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -2,12 +2,14 @@ package com.lukaroncevic.spring6restmvc.services;
 
 import com.lukaroncevic.spring6restmvc.model.BeerCSVRecord;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
 
     @Override
