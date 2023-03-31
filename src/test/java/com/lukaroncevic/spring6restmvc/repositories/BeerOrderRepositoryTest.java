fcc7d203a60bc1cd715e1852f5bb2c90be414373
lroncevic <roncevicluka@outlook.com>
2023-03-31 16:38:12 +0200

    Added beer order repository

---
+++ src/test/java/com/lukaroncevic/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.lukaroncevic.spring6restmvc.repositories;
+
+import com.lukaroncevic.spring6restmvc.entities.Beer;
+import com.lukaroncevic.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().get(0);
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Test
+    void beerOrder() {
+        System.out.println(beerOrderRepository.count());
+        System.out.println(customerRepository.count());
+        System.out.println(beerRepository.count());
+        System.out.println(testCustomer.getName());
+        System.out.println(testBeer.getBeerName());
+    }
+}
