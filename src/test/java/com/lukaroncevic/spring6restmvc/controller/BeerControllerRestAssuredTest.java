d3a58114cf2d4d8ffb20703d418fdb02dede9e43
lroncevic <roncevicluka@outlook.com>
2023-04-10 12:08:55 +0200

    Configured spring security for RestAssured test

--- src/test/java/com/lukaroncevic/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/com/lukaroncevic/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -6,11 +6,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
+@ComponentScan(basePackages = "com.lukaroncevic.spring6restmvc")
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BeerControllerRestAssuredTest {
+
+    @Configuration
+    public static class TestConfig{
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests().anyRequest().permitAll();
+
+            return http.build();
+        }
+    }
 
     @LocalServerPort
     Integer localPort;
